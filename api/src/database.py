6e08be903f8780e9ce4ad0304134e2019046256c
Daniel Maike <danmke@hotmail.com>
2022-03-26 19:10:00 -0300

    Added create_tables in Database class
    Signed-off-by: Daniel Maike <danmke@hotmail.com>

--- api/src/database.py
+++ api/src/database.py
@@ -9,7 +9,10 @@ class Database():
                             user=os.environ['POSTGRESQL_USERNAME'],
                             password=os.environ['POSTGRESQL_PASSWORD']):
         self.conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+
         self.cur = self.conn.cursor()
+
+        self.create_tables()
 
     def query(self, query):
         try:
@@ -24,3 +27,48 @@ class Database():
     def close(self):
         self.cur.close()
         self.conn.close()
+
+    def create_tables(self):
+        command = """
+            CREATE TABLE IF NOT EXISTS CUSTOMER (
+                email VARCHAR(255) NOT NULL,
+                fullName VARCHAR(255) NOT NULL,
+                phone VARCHAR(15) NOT NULL,
+                cpf VARCHAR(14) NOT NULL,
+                birthDate DATE NOT NULL,
+                active BOOLEAN NOT NULL,
+                CONSTRAINT CUSTOMER_UK UNIQUE (cpf),
+                CONSTRAINT CUSTOMER_PK PRIMARY KEY (email)
+            );
+
+            CREATE TABLE IF NOT EXISTS DISC (
+                discName VARCHAR(255) NOT NULL,
+                artist VARCHAR(255) NOT NULL,
+                releaseYear SMALLINT NOT NULL,
+                musicStyle VARCHAR(255) NOT NULL,
+                availableQuantity INTEGER NOT NULL,
+                unitaryValue DOUBLE PRECISION NOT NULL,
+                CONSTRAINT DISC_PK PRIMARY KEY (discName, artist)
+            );
+
+            CREATE TABLE IF NOT EXISTS PURCHASE (
+                purchaseId SERIAL NOT NULL,
+                purchaseDateTime TIMESTAMP NOT NULL,
+                totalValue DOUBLE PRECISION NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                effective BOOLEAN NOT NULL,
+                CONSTRAINT PURCHASE_PK PRIMARY KEY (purchaseId),
+                CONSTRAINT PURCHASE_CUSTOMER_FK FOREIGN KEY (email) REFERENCES CUSTOMER (email) ON DELETE RESTRICT
+            );
+
+            CREATE TABLE IF NOT EXISTS has (
+                purchaseId SERIAL NOT NULL,
+                discName VARCHAR(255) NOT NULL,
+                artist VARCHAR(255) NOT NULL,
+                quantity INTEGER NOT NULL,
+                CONSTRAINT has_PURCHASE_FK FOREIGN KEY (purchaseId) REFERENCES PURCHASE (purchaseId) ON DELETE RESTRICT,
+                CONSTRAINT has_DISC_FK FOREIGN KEY (discName, artist) REFERENCES DISC (discName, artist) ON DELETE RESTRICT
+            );
+        """
+        self.query(command)
+        self.commit()
